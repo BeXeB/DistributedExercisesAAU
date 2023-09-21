44c48ae56d3023076816650759467e13a1bd1735
Bence Imre Kiss <kissbenceimre99@gmail.com>
2023-09-21 13:22:26 +0200

    ex 1 and 2

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -23,9 +23,77 @@ class Gossip(Device):
         self._secrets = set([index])
 
     def run(self):
-        # the following is your termination condition, but where should it be placed?
-        # if len(self._secrets) == self.number_of_devices():
-        #    return
+        # we send a message to the last device in the network unless we are the last device
+        if self.index() < self.number_of_devices() - 1:
+            self.medium().send(GossipMessage(
+                self.index(), self.number_of_devices() - 1, self._secrets))
+
+        # we wait for a message
+        while True:
+            message = self.medium().receive()
+            if message is None:
+                continue
+
+            # we add the secrets from the message to our own set
+            self._secrets.update(message.secrets)
+
+            # if we are the last device, we wait until we have all the secrets then we send it to everyone else
+            if self.index() == self.number_of_devices() - 1:
+                if len(self._secrets) == self.number_of_devices():
+                    for i in range(self.number_of_devices()):
+                        if i != self.index():
+                            self.medium().send(GossipMessage(self.index(), i, self._secrets))
+                    break
+
+            # if we are not then we wait for the secrets
+            if len(self._secrets) == self.number_of_devices():
+                break
+
+        return
+
+    def print_result(self):
+        print(f'\tDevice {self.index()} got secrets: {self._secrets}')
+
+
+class GossipCircle(Device):
+
+    def __init__(self, index: int, number_of_devices: int, medium: Medium):
+        super().__init__(index, number_of_devices, medium)
+        # for this exercise we use the index as the "secret", but it could have been a new routing-table (for instance)
+        # or sharing of all the public keys in a cryptographic system
+        self._secrets = set([index])
+
+    def run(self):
+        # we send a message to the next device if we are the first device
+        if self.index() == 0:
+            self.medium().send(GossipMessage(self.index(), (self.index() + 1) %
+                                             self.number_of_devices(), self._secrets))
+
+        # we wait for a message
+        while True:
+            message = self.medium().receive()
+            if message is None:
+                continue
+
+            # we add the secrets from the message to our own set
+            self._secrets.update(message.secrets)
+
+            # if we are the last device, we wait until we have all the secrets then we send it to everyone else
+            if self.index() == self.number_of_devices() - 1:
+                if len(self._secrets) == self.number_of_devices():
+                    for i in range(self.number_of_devices()):
+                        if i != self.index():
+                            self.medium().send(GossipMessage(self.index(), i, self._secrets))
+                    break
+            else:
+                # if we are not then we wait for the secrets
+                if len(self._secrets) == self.number_of_devices():
+                    break
+
+            # we send the message to the next device
+            self.medium().send(GossipMessage(self.index(), (self.index() + 1) %
+                                             self.number_of_devices(), self._secrets))
+
         return
 
     def print_result(self):
--- exercises/exercise2.py
+++ exercises/exercise2.py
@@ -11,6 +11,7 @@ class RipMessage(MessageStub):
     def __str__(self):
         return f'RipMessage: {self.source} -> {self.destination} : {self.table}'
 
+
 class RoutableMessage(MessageStub):
     def __init__(self, sender: int, destination: int, first_node: int, last_node: int, content):
         super().__init__(sender, destination)
@@ -22,14 +23,14 @@ class RoutableMessage(MessageStub):
         return f'RoutableMessage: {self.source} -> {self.destination} : {self.content}'
 
 
-
-
 class RipCommunication(Device):
 
     def __init__(self, index: int, number_of_devices: int, medium: Medium):
         super().__init__(index, number_of_devices, medium)
-        
-        self.neighbors = [] # generate an appropriate list
+
+        # generate an appropriate list
+        self.neighbors = [(self.index() + number_of_devices - 1) %
+                          number_of_devices, (self.index() + 1) % number_of_devices]
 
         self.routing_table = dict()
 
@@ -45,34 +46,60 @@ class RipCommunication(Device):
             if ingoing is None:
                 # this call is only used for synchronous networks
                 self.medium().wait_for_next_round()
+                if self.routing_table_complete():
+                    break
                 continue
 
             if type(ingoing) is RipMessage:
-                print(f"Device {self.index()}: Got new table from {ingoing.source}")
-                returned_table = self.merge_tables(ingoing.source, ingoing.table)
+                print(
+                    f"Device {self.index()}: Got new table from {ingoing.source}")
+                returned_table = self.merge_tables(
+                    ingoing.source, ingoing.table)
                 if returned_table is not None:
                     self.routing_table = returned_table
                     for neigh in self.neighbors:
                         self.medium().send(RipMessage(self.index(), neigh, self.routing_table))
 
             if type(ingoing) is RoutableMessage:
-                print(f"Device {self.index()}: Routing from {ingoing.first_node} to {ingoing.last_node} via #{self.index()}: [#{ingoing.content}]")
+                print(
+                    f"Device {self.index()}: Routing from {ingoing.first_node} to {ingoing.last_node} via #{self.index()}: [#{ingoing.content}]")
                 if ingoing.last_node is self.index():
-                    print(f"\tDevice {self.index()}: delivered message from {ingoing.first_node} to {ingoing.last_node}: {ingoing.content}")
+                    print(
+                        f"\tDevice {self.index()}: delivered message from {ingoing.first_node} to {ingoing.last_node}: {ingoing.content}")
                     continue
                 if self.routing_table[ingoing.last_node] is not None:
-                    (next_hop, distance) = self.routing_table[ingoing.last_node]
-                    self.medium().send(RoutableMessage(self.index(), next_hop, ingoing.first_node, ingoing.last_node, ingoing.content))
+                    (next_hop,
+                     distance) = self.routing_table[ingoing.last_node]
+                    self.medium().send(RoutableMessage(self.index(), next_hop,
+                                                       ingoing.first_node, ingoing.last_node, ingoing.content))
                     continue
-                print(f"\tDevice {self.index()}:  DROP Unknown route #{ingoing.first_node} to #{ingoing.last_node} via #{self.index}, message #{ingoing.content}")
+                print(
+                    f"\tDevice {self.index()}:  DROP Unknown route #{ingoing.first_node} to #{ingoing.last_node} via #{self.index}, message #{ingoing.content}")
 
             # this call is only used for synchronous networks
             self.medium().wait_for_next_round()
 
     def merge_tables(self, src, table):
-        # return None if the table does not change
-        pass
+        temp_table = self.routing_table.copy()
+        for key, (next_hop, distance) in table.items():
+            if key not in temp_table:
+                temp_table[key] = (src, distance + 1)
+                continue
+            if distance + 1 < temp_table[key][1]:
+                temp_table[key] = (src, distance + 1)
+        if temp_table == self.routing_table:
+            return None
+        return temp_table
 
+    def routing_table_complete(self):
+        if len(self.routing_table) < self.number_of_devices()-1:
+            return False
+        for row in self.routing_table:
+            (next_hop, distance) = self.routing_table[row]
+            if distance > (self.number_of_devices() / 2):
+                return False
+            return True
 
     def print_result(self):
-        print(f'\tDevice {self.index()} has routing table: {self.routing_table}')
+        print(
+            f'\tDevice {self.index()} has routing table: {self.routing_table}')
